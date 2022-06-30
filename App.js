e5bfce948bd22851254cd4f97bacd9fa0d6c8813
Rafael <rafaellf112@gmail.com>
2022-06-29 21:52:21 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = ()=>  setToggle(oldToggle =>  !oldToggle);
+
+  useEffect(()=>{
+    //Liga flash do celular
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(()=>{
+    //Quando chacoalhar o celular ele liga ou desliga a lanterna
+    const subscription = RNShake.addListener(()=>{
+      setToggle(oldToggle =>  !oldToggle);
+    });
+    // Essa func vai ser chamada quando o componente for ser desmontado
+    return() => subscription.remove();
+  },[]);
+
+  //toggle para trocar entre modos
+  return <View style={toggle ? style.containerLight : style.container}>
+    <TouchableOpacity 
+      onPress={handleChangeToggle}>
+      <Image 
+        style={toggle ?style.lightingOn : style.lightingOff}source={
+        toggle ? require('./assets/icons/eco-light.png'):require('./assets/icons/eco-light-off.png')}
+      />
+      <Image 
+        style={style.dioLogo}
+        source={
+        toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png')}
+      />
+    </TouchableOpacity>
+  </View>;
+};
+
+export default App;
+
+const style =StyleSheet.create({
+  container : {
+    flex: 1,
+    backgroundColor : 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight : {
+    flex: 1,
+    backgroundColor : 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  },
+  dioLogo: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width:230,
+    height:230,
+  },
+});
